c79fa8f891d52aba0c715313bc68b1f3ed1279b1
Fawwaz Syams <fawwazsyams04@gmail.com.com>
2026-01-28 16:59:01 +0700

    Add Procfile and update app.py for deployment configuration

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Penting untuk React
 import pickle
+import os
 
 app = Flask(__name__)
 CORS(app) 
@@ -23,4 +24,5 @@ def predict():
     return jsonify({'price': round(prediction, 0)})
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
